e2a07dac9533e80981c64716b61b512820c28f21
RedRastaRigger <87661155+RedRastaRigger@users.noreply.github.com>
2022-11-23 07:17:55 +0100

    added mvp

---
+++ Src/main.py
@@ -0,0 +1,45 @@
+
+from colorama import init,Fore, Back,Style
+
+
+init(autoreset=True)
+
+
+print(Style.BRIGHT+Fore.YELLOW+"enter first text:")
+first_text  = input()
+print(Style.BRIGHT+Fore.YELLOW+"enter second text:")
+second_text = input()
+
+to_itterate = len(first_text) if len(first_text) >= len(second_text) else len(second_text)
+
+second_print = ""
+first_print  = ""
+
+
+
+for char_nr in range(to_itterate):
+
+    if len(first_text) > char_nr  : first_char  = first_text[char_nr]
+    else                          : first_char  = ""
+    if len(second_text) > char_nr : second_char  = second_text[char_nr]
+    else                          : second_char  = ""
+    
+
+    if first_char == second_char :
+        first_print  += Style.NORMAL+Fore.WHITE+first_char
+        second_print += Style.NORMAL+Fore.WHITE+first_char
+    else:
+        first_print  += Style.BRIGHT+Fore.YELLOW+first_char
+        second_print += Style.BRIGHT+Fore.GREEN+second_char
+
+
+
+print()
+print(first_print)
+print("---------------------")
+print(second_print)
+print()
+
+
+
+
